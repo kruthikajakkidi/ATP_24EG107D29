8fd1dad4c02d3af55e60ba41f8e4b6f2692d2e1c
Kruthika <kruthikajakkidi@gmail.com>
2026-05-07 12:00:00 +0530

    db

--- week6/Employee/backend/server.js
+++ week6/Employee/backend/server.js
@@ -8,7 +8,8 @@ const app = exp();
 
 // CORS middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin:["https://employees-sand.vercel.app" || "http://localhost:5173"],
+  credentials:true
 }));
 
 // Body parser middleware
