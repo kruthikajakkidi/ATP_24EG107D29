1e732f20e6fb4b037cf565f04764c59a0e2f2ac3
kruthikajakkidi <kruthikajakkidi@gmail.com>
2026-05-18 13:07:17 +0530

    Refactor Home component with new layout and link
    Updated Home component layout and added navigation link.

--- week6/Employee/frontend/src/components/Home.jsx
+++ week6/Employee/frontend/src/components/Home.jsx
@@ -1,12 +1,25 @@
-import { useContext } from "react"
-import { counterContextObj } from "../contexts/ContextProvider"
-
+import { NavLink } from "react-router";
+import CreateEmp from "./CreateEmp";
 function Home() {
- 
- return(
-  <div className="text-center bg-amber-300">Welcome to Employee Data System</div>
-   
-  )
+  return (
+    <div className="min-h-[80vh] flex justify-center items-center bg-gray-100 p-8">
+      <div className="bg-white p-12 rounded-2xl shadow-2xl text-center w-[80%] max-w-4xl">
+        
+        <h1 className="text-5xl font-bold text-yellow-500 mb-6">
+          Employee Management System
+        </h1>
+
+        <p className="text-xl text-gray-600 leading-9 mb-8">
+         Full-Stack application to track Employee details.
+        </p>
+
+        <NavLink to="create-emp" className="px-8 py-4 text-lg bg-yellow-500 text-white rounded-xl hover:bg-yellow-600 transition duration-300">
+          Get Started
+        </NavLink>
+
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
